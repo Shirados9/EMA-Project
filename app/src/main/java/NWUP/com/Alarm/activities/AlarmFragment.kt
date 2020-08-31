1957d6493d49797bbda001c5c0fa35ff95ba4316
Darwin Schulz <darwinschulz@outlook.com>
2020-08-31 14:39:03 +0200

    Edit comment in AlarmFragment.kt

--- app/src/main/java/NWUP/com/Alarm/activities/AlarmFragment.kt
+++ app/src/main/java/NWUP/com/Alarm/activities/AlarmFragment.kt
@@ -7,13 +7,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-//creates the fragment with a navigator
+/**
+ * creates the fragment with a navigator
+ * Alarm is based on https://github.com/learntodroid/SimpleAlarmClock/tree/master/app which
+ * is programmed in java. We converted it to Kotlin and changed it, so it suits our purposes
+ */
 class AlarmFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_alarm_main, container, false)
     }
 }
